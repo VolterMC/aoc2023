f5c69ff3b92a40eb0c94549605eca21a0061f474
Bartosz Rosadzinski <voltermc@gmail.com>
2023-12-01 16:58:00 +0100

    Initial commit
    Implemented a template for input loading.
    Created Day 01 stub.

---
+++ src/day01.lua
@@ -0,0 +1,10 @@
+-- INPUT READING BEGINS HERE
+if small_input then
+    local input_file = io.open("day01_input_small.txt", r)
+    io.input(input_file)
+else
+    local input_file = io.open("day01_input_large.txt", r)
+    io.input(input_file)
+end
+
+-- PROBLEM IMPLEMENTATION BEGINS HERE
